f9c443f073080a1f93f9c92c3020e0b84ab6051f
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-18 15:45:03 +0200

    feat: Add RepeatOffIcon component to icons library, enhancing SVG icon collection

---
+++ src/components/icons/repeat-off-icon.tsx
@@ -0,0 +1,24 @@
+import type { IconProps } from './type';
+import { ICON_SIZE } from './constants';
+
+export function RepeatOffIcon(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={ICON_SIZE}
+      height={ICON_SIZE}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 12v-3c0 -1.336 .873 -2.468 2.08 -2.856m3.92 -.144h10m-3 -3l3 3l-3 3" />
+      <path d="M20 12v3a3 3 0 0 1 -.133 .886m-1.99 1.984a3 3 0 0 1 -.877 .13h-13m3 3l-3 -3l3 -3" />
+      <path d="M3 3l18 18" />
+    </svg>
+  );
+}
